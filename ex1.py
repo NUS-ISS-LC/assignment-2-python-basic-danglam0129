58f319b26714cc8ad0c847a10dd6b8772c6313b3
“Dang <danglam0129@gmail.com>
2025-11-09 22:37:37 +0800

    assignment 2

--- ex1.py
+++ ex1.py
@@ -1,3 +1,16 @@
-def find(s, n):
+def find(nums, target):
 # write your implementation here
+    nums_dictionary = {}
+
+    for i in range(len(nums)):
+        difference = target - nums[i]
+        if difference in nums_dictionary:
+            return [i, nums_dictionary[difference]]
+        
+        nums_dictionary[nums[i]] = i
+
     return None
+
+
+print(find([2,7,11,15], 9))
+print(find([3,2,4], 6))
